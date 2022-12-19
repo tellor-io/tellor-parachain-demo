3fee567cba7b4f100753656b90b9d2b564f0848f
Frank Bell <frank@parity.io>
2022-12-19 16:36:15 +0000

    build: improve build/launch scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,22 @@
+#!/bin/bash
+
 echo Building Polkadot...
-cd polkadot
+cd polkadot || exit
 cargo build --release
 cd ..
 
 echo Building Moonbeam...
-cd moonbeam
+cd moonbeam || exit
 git checkout runtime-2000
 cargo build --release
 cd ..
 
 echo Building consumer parachain...
-cd substrate-parachain-node
+cd substrate-parachain-node || exit
 cargo build --release
 cd ..
 
 echo Building Tellor contracts...
-cd tellor-contracts
+cd tellor-contracts || exit
 forge build
 cd ..
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,2 +1,23 @@
+#!/bin/bash
+
+ZOMBIENET_VERSION=v1.3.23
+
+# Check if zombienet needs to be downloaded
+if [ ! -e zombienet ]; then
+  case $(uname) in
+    "Darwin") ARCHITECTURE=macos;;
+    "Linux")
+    {
+      # todo: test on linux
+      ARCHITECTURE=linux
+      if $(uname -m) = x86_64; then ARCHITECTURE=$ARCHITECTURE-x64; else ARCHITECTURE=$ARCHITECTURE-arm64; fi
+    } ;;
+    esac
+
+  echo Downloading zombienet $ZOMBIENET_VERSION...
+  curl -s -L https://github.com/paritytech/zombienet/releases/download/$ZOMBIENET_VERSION/zombienet-$ARCHITECTURE > zombienet
+  chmod +x zombienet
+fi
+
 echo Launching network...
-./zombienet-macos spawn -p native network.toml
+./zombienet spawn -p native network.toml

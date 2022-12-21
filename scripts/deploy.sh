36b006ef9a7f7e114afcb4e6a68f6601ba12ab2d
Frank Bell <frank@parity.io>
2022-12-21 21:51:20 +0000

    fix: correct rpc port in network config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ echo Building Tellor contracts...
 cd tellor-contracts || exit
 forge build
 echo Deploying Tellor contracts to Moonbeam...
-forge create --rpc-url http://localhost:9931 --constructor-args 0xFFFFFFFFxcTok3nAddr355 \
+forge create --rpc-url http://localhost:9921 \
   --private-key 0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133 --legacy src/Staking.sol:Staking
 cd ..

abd925894e907bbd2f381c7cf2df77405da561b6
TriplEight <denis.pisarev@parity.io>
2023-05-10 14:52:30 +0200

    install from branch mentioned in README

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-PIT=parachains-integration-tests#frank/additional-keypair-types
-PIT_PACKAGE=@parity/$PIT
+PIT=https://github.com/paritytech/parachains-integration-tests.git#frank/additional-keypair-types
+PIT_PACKAGE=parachains-integration-tests
 
 # Ensure yarn available
 if ! which yarn &> /dev/null
 then
-echo Error: could not find yarn
-exit
+  echo Error: could not find yarn
+  exit
 fi
 
 # Install parachains-integration-tests
 if ! which $PIT &> /dev/null
 then
-echo Installing parachain integration framework...
-yarn global add --silent  --no-progress $PIT_PACKAGE
-echo Use \'yarn global remove "$PIT_PACKAGE"\' to remove...
-  if ! which $PIT &> /dev/null
+  echo Installing parachain integration framework...
+  yarn global add $PIT
+  echo Use \'yarn global remove "$PIT_PACKAGE"\' to remove...
+  if ! which $PIT_PACKAGE &> /dev/null
   then
-    echo Error: could not find \'"$PIT"\' after global install - ensure that the path reported by \'yarn global bin\' is in your PATH.
+    echo Error: could not find \'"$PIT_PACKAGE"\' after global install - ensure that the path reported by \'yarn global bin\' is in your PATH.
     exit
   fi
 fi
